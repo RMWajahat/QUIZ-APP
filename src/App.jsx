dd02277466c716a241812c0aea039a2cc92ffe45
uzair rana <you@example.com>
2024-03-29 14:23:43 +0500

    Committing local changes before merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import SignUp from './pages/SignUp';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Quiz from './pages/Quiz';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <BrowserRouter>
    <Routes>
+    <Route path='/quiz' element={<Quiz/>}></Route>
     <Route path='/signup' element={<SignUp/>}></Route>
     <Route path='/login' element={<Login/>}></Route>
     <Route path='/' element={<Home/>}></Route>
